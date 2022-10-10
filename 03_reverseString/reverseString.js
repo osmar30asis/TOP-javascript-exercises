4d300ac2b2933da6e3b167c0ab594bec0b9601ab
osmaryupanqui <osmar30asis@gmail.com>
2022-10-09 20:57:12 -0500

    added solutions to js and spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let chars = string.length
+    let text = ''
+    for (let i = 0; i < chars; i++) {
+        text += string[chars - i - 1]
+    }
+    return text
 };
-
+console.log('hi'.length)
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const arrayReturn = [];
 
-};
-
+    array.forEach(function (element) {
+        if (!args.includes(element)) {
+            arrayReturn.push(element);
+        }
+    })
+    return arrayReturn;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
