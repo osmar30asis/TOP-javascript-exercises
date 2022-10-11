cdd91d5686d764b5b1040fc1922e819fab55c4e8
osmaryupanqui <osmar30asis@gmail.com>
2022-10-10 23:14:21 -0500

    added solutions for exercises 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let conversion
+  conversion = (Number(fahrenheit) - 32) * (5 / 9)
+  return Math.round(conversion * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let conversion
+  conversion = (Number(celcius) * (9 / 5)) + 32
+  return Math.round(conversion * 10) / 10 
 };
 
 // Do not edit below this line
